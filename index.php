8a3eb7e765f76019176f586f9a7d7726389cab6c
Efe TOKER <efetoker@gmail.com>
2023-05-15 20:37:34 +0300

    init

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "Control is an illusion.";
+echo "<span style='font-family: ui-monospace, 'Cascadia Code', 'Source Code Pro', Menlo, Consolas, 'DejaVu Sans Mono', monospace;'>"."Control is an illusion."."</span>";
