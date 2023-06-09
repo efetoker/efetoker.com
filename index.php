70a1e0ebc9b19350753b62b3c48364b2343c2413
Efe TOKER <efetoker@gmail.com>
2023-06-09 12:23:08 +0300

    up

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$font = "ui-monospace, 'Cascadia Code', 'Source Code Pro', Menlo, Consolas, 'DejaVu Sans Mono', monospace";
-
-echo '<span style="font-family: ' . $font . ';">Control is an illusion.</span>';
